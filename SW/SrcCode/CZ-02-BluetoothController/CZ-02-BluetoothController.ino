5818e90ba2ddbdf70c324679e00d9ffe0c5a7c69
Vignesh S <vignura@gmail.com>
2018-12-03 06:19:31 +0530

    Added self test function

--- SW/SrcCode/CZ-02-BluetoothController/CZ-02-BluetoothController.ino
+++ SW/SrcCode/CZ-02-BluetoothController/CZ-02-BluetoothController.ino
@@ -1,9 +1,113 @@
+ /***********************************************************************************************/
+/*
+ *  \file       : CZ-02-BluetoothController.ino
+ *  \date       : 02-DEC-2018 
+ *  \author     : Vignesh S 
+ *  \email      : vignura@gmail.com
+ *  \copyright  : All Rights are Reserved 
+/*
+/***********************************************************************************************/
+// headers
+
+/*************************************** Pin Mappings ******************************************/
+// ground plane
+#define GND_PLANE_PIN		PIN_A4
+
+// user LEDS
+#define USR_LED_1				PIN_A2
+#define USR_LED_2				PIN_A3
+
+// HC-05 
+#define HC05_ST					PIN_A1		/* status pin */
+#define HC05_EN					PIN_A5		/* enable pin */
+
+// Relay 
+#define RELAY 					PIN_A0
+/***********************************************************************************************/
+
+#define SELF_TEST_COUNT			0x01
+/***********************************************************************************************/
+/*! 
+* \fn         :: setup()
+* \author     :: Vignesh S
+* \date       :: 02-DEC-2018
+* \brief      :: This function performs the following initializations
+*                1) Sets the Relay pins as output and sets its state to low
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
 void setup() {
-  // put your setup code here, to run once:
+  
+  // Configure the Ground plane pin as input
+  // do not change this pin sate after initialization
+  pinMode(GND_PLANE_PIN, INPUT);
+
+  // LED initialization
+	pinMode(USR_LED_1, OUTPUT);
+	pinMode(USR_LED_2, OUTPUT);
+
+	// Relay initialization
+	pinMode(RELAY, OUTPUT);
+
+	// perform self test
+	SelfTest(SELF_TEST_COUNT);
 
 }
 
+/***********************************************************************************************/
+/*! 
+* \fn         :: loop()
+* \author     :: Vignesh S
+* \date       :: 02-DEC-2018
+* \brief      :: This functions is called repeatedly from a infinite for loop in main().
+*                It does the following functions
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
 void loop() {
-  // put your main code here, to run repeatedly:
 
+	// check for any commands
+
+	// if any command is received, process it
+
+	// write the Relay ON time to EEPROM 
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: SelfTest()
+* \author     :: Vignesh S
+* \date       :: 02-DEC-2018
+* \brief      :: This function perform selftest of onboard peripherals
+* \param[in]  :: iTestCount (number of times to repeat the test)
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void SelfTest(int iTestCount)
+{
+	int iCount = 0;
+
+	for(iCount = 0; iCount < iTestCount; iCount++)
+	{
+		// turn on the two LEDs
+		digitalWrite(USR_LED_1, HIGH);
+		digitalWrite(USR_LED_1, HIGH);
+		// turn on Relay
+		digitalWrite(RELAY, HIGH);
+
+		delay(2000);
+		
+		// turn off the two LEDs
+		digitalWrite(USR_LED_1, LOW);
+		digitalWrite(USR_LED_1, LOW);
+		// turn off Relay
+		digitalWrite(RELAY, LOW);
+
+		delay(500);		
+	}
 }
